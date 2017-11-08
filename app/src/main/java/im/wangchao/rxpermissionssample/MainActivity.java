32c82ec38569a434841c8ea6f9aa3c2661e4f186
wangchao <magician.of.technique@gmail.com>
2017-11-08 13:31:20 +0800

    [*] add sample

--- app/src/main/java/im/wangchao/rxpermissionssample/MainActivity.java
+++ app/src/main/java/im/wangchao/rxpermissionssample/MainActivity.java
@@ -1,13 +1,59 @@
 package im.wangchao.rxpermissionssample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import im.wangchao.rxpermissions.RxPermissions;
+import io.reactivex.Observable;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private RxPermissions mPermissions;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.one).setOnClickListener(this.click());
+        findViewById(R.id.two).setOnClickListener(this.click());
+        mPermissions = RxPermissions.with(this);
+    }
+
+    private View.OnClickListener click(){
+        return v -> {
+            switch (v.getId()){
+                case R.id.one:
+                    requestOne();
+                    break;
+                case R.id.two:
+                    requestTwo();
+                    break;
+            }
+        };
+    }
+
+    private void requestOne(){
+        mPermissions.requestWithRationale(
+                "this is rationale.",
+                "this is never ask again prompt.",
+                Manifest.permission.CAMERA)
+                .subscribe(result -> {
+                   Log.e("wcwcwc", "request result: " + result);
+                });
+    }
+
+    private void requestTwo(){
+        String event = "some event.";
+        Observable.just(event)
+                .compose(mPermissions.ensureWithRationale(
+                        "this is rationale.",
+                        "this is never ask again prompt.",
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE))
+                .subscribe(result -> {
+                   // result is event
+                    Log.e("wcwcwc", "request success: " + result);
+                });
     }
 }
